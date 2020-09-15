bf8d4e85a84f3b171d64931b82b0d53c68d86f86
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-15 06:48:05 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 export default class EyesOnMe extends Component {
-    focus = () => console.log('good')
+    focus = () => console.log('Good')
     blur = () => console.log('Hey! Eyes on me!')
     render() {
         return (
